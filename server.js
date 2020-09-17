aba06d626301f50938dfc92691d05c431324e68f
alertguo <1196614612@qq.com>
2020-09-17 21:32:14 +0800

    实现用户注册功能，并储存数据，注册成功跳转到登录页面

--- server.js
+++ server.js
@@ -23,33 +23,62 @@ var server = http.createServer(function (request, response) {
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-  response.statusCode = 200
-  // 默认首页
-  const filePath = path === '/' ? '/index.html' : path
-  const index = filePath.lastIndexOf('.')
-  // suffix 是后缀
-  const suffix = filePath.substring(index)
-  // 哈希表替换
-  const fileTypes = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'text/javascript',
-    '.png': 'image/png',
-    '.img': 'image/jpeg',
+  if (path === '/register' && method === 'POST') {
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    const userArray = JSON.parse(fs.readFileSync('./db/users.json'))
+    console.log(userArray)
+    const array = []
+    request.on('data', (chunk) => {
+      array.push(chunk)
+    }) // 请求一个数据，将其添加到数组
+    request.on('end', () => {
+      const string = Buffer.concat(array).toString()
+      // 不用buffer，直接返回数组，返回的是字符编码
+      const obj = JSON.parse(string)
+      console.log(obj.name)
+      console.log(obj.password)
+      const lastUser = userArray[userArray.length - 1]
+      const newUser = {
+        // id 为最后一个用户的 id+1,如果lastUser不存在id为1
+        id: lastUser ? lastUser.id + 1 : 1,
+        name: obj.name,
+        password: obj.password,
+      } // 要得到userArray里id最大的+1，一般是最后一个
+      userArray.push(newUser)
+      console.log(userArray)
+      // 将添加了数据后的数组序列化后返回到 users.json 文件中
+      fs.writeFileSync('./db/users.json',JSON.stringify(userArray))
+      response.end()
+    }) // 请求结束，打印出这个数组
+  } else {
+    response.statusCode = 200
+    // 默认首页
+    const filePath = path === '/' ? '/index.html' : path
+    const index = filePath.lastIndexOf('.')
+    // suffix 是后缀
+    const suffix = filePath.substring(index)
+    // 哈希表替换
+    const fileTypes = {
+      '.html': 'text/html',
+      '.css': 'text/css',
+      '.js': 'text/javascript',
+      '.png': 'image/png',
+      '.img': 'image/jpeg',
+    }
+    response.setHeader(
+      'Content-Type',
+      `${fileTypes[suffix] || 'text/html'};charset=utf-8`
+    )
+    let content
+    try {
+      content = fs.readFileSync(`./public${filePath}`)
+    } catch (error) {
+      content = '文件不存在'
+      response.statusCode = 404
+    }
+    response.write(content)
+    response.end()
   }
-  response.setHeader(
-    'Content-Type',
-    `${fileTypes[suffix] || 'text/html'};charset=utf-8`
-  )
-  let content
-  try {
-    content = fs.readFileSync(`./public${filePath}`)
-  } catch (error) {
-    content = '文件不存在'
-    response.statusCode = 404
-  }
-  response.write(content)
-  response.end()
 
   /******** 代码结束，下面不要看 ************/
 })
--- test.js
+++ test.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 
 // 读数据库
 const usersString = fs.readFileSync('./db/users.json').toString()
-const usersArray = JSON.parse(usersString)
+const usersArray = JSON.parse(usersString) // 反序列化得到数组
 
 // 写数据库
-const user3 = {id:3,name:'tom',password:'yyy',age:22}
+const user3 = { id: 3, name: 'tom', password: 'yyy', age: 22 }
 usersArray.push(user3)
-const string = JSON.stringify(usersArray)
-fs.writeFileSync('./db/users.json',string)
+const string = JSON.stringify(usersArray) // 序列化得到字符串
+fs.writeFileSync('./db/users.json', string)
