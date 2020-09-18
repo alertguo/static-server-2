3c56edc716f86fc08a4e72979990270de8632b61
alertguo <1196614612@qq.com>
2020-09-18 22:43:58 +0800

    登录成功home.html显示用户名登录成功，并且把信息隐藏在服务器

--- server.js
+++ server.js
@@ -22,11 +22,65 @@ var server = http.createServer(function (request, response) {
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+  const session = JSON.parse(fs.readFileSync('./session.json').toString()) // 读session
 
-  if (path === '/register' && method === 'POST') {
+  if (path === '/sign_in' && method === 'POST') {
+    const userArray = JSON.parse(fs.readFileSync('./db/users.json'))
+    const array = []
+    request.on('data', (chunk) => {
+      array.push(chunk)
+    }) // 请求一个数据，将其添加到数组
+    request.on('end', () => {
+      const string = Buffer.concat(array).toString()
+      // 不用buffer，直接返回数组，返回的是字符编码
+      const obj = JSON.parse(string) // name password
+      const user = userArray.find(
+        (user) => user.name === obj.name && user.password === obj.password
+      )
+      if (user === undefined) {
+        response.statusCode = 400
+        response.setHeader('Content-Type', 'text/json;charset=utf-8')
+        response.end(`{"errorCode": 531}`)
+      }
+      response.statusCode = 200
+      const random = Math.random()
+      session[random] = { user_id: user.id }
+      fs.writeFileSync('./session.json', JSON.stringify(session)) // 存session
+      response.setHeader('Set-Cookie', `session_id=${random};HttpOnly`)
+      response.end()
+    })
+  } else if (path === '/home.html') {
+    const cookie = request.headers['cookie']
+    let sessionId
+    try {
+      sessionId = cookie
+        .split(';')
+        .filter((s) => s.indexOf('session_id=') >= 0)[0]
+        .split('=')[1]
+    } catch (error) {}
+    if (sessionId && session[sessionId]) {
+      const userId = session[sessionId].user_id
+      const userArray = JSON.parse(fs.readFileSync('./db/users.json')) // 先找到所有的user
+      const user = userArray.find((user) => user.id === userId) // 在所有user里找id等于userId的
+      const homeHtml = fs.readFileSync('./public/home.html').toString()
+      let string = ''
+      if (user) {
+        string = homeHtml
+          .replace('{{loginStatus}}', '已登录')
+          .replace('{{user.name}}', user.name)
+      }
+      response.write(string)
+    } else {
+      const homeHtml = fs.readFileSync('./public/home.html').toString()
+      const string = homeHtml
+        .replace('{{loginStatus}}', '未登录')
+        .replace('{{user.name}}', '')
+      response.write(string)
+    }
+    response.end()
+  } else if (path === '/register' && method === 'POST') {
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     const userArray = JSON.parse(fs.readFileSync('./db/users.json'))
-    console.log(userArray)
     const array = []
     request.on('data', (chunk) => {
       array.push(chunk)
@@ -45,9 +99,8 @@ var server = http.createServer(function (request, response) {
         password: obj.password,
       } // 要得到userArray里id最大的+1，一般是最后一个
       userArray.push(newUser)
-      console.log(userArray)
       // 将添加了数据后的数组序列化后返回到 users.json 文件中
-      fs.writeFileSync('./db/users.json',JSON.stringify(userArray))
+      fs.writeFileSync('./db/users.json', JSON.stringify(userArray))
       response.end()
     }) // 请求结束，打印出这个数组
   } else {
